1e9afa2202db09fd066f61ac386134c7029e7301
Yaroslav Bochkov <cruelty86@yandex.ru>
2025-08-08 11:55:44 +0300

    Удалён запуск через app.run() для деплоя на Render

--- backend/app.py
+++ backend/app.py
@@ -280,5 +280,5 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
-if __name__ == "__main__":
-    app.run(debug=False)
+# if __name__ == "__main__":
+    # app.run(debug=False)
